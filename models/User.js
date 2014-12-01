5532b6727cc139085e38611c77eab985fd7ae093
John Cardwell <cardwell.john@gmail.com>
2014-11-30 18:24:37 -0800

    make adding a ppastor dynamic and sortable. huzzah

--- models/User.js
+++ models/User.js
@@ -1,24 +1,29 @@
 var keystone = require('keystone'),
-	Types = keystone.Field.Types;
+    Types = keystone.Field.Types;
 
 /**
  * User Model
  * ==========
  */
 
-var User = new keystone.List('User');
+var User = new keystone.List('User',{
+    sortable: true
+});
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true }
+    name: { type: Types.Name, required: true, index: true },
+    email: { type: Types.Email, initial: true, required: true, index: true },
+    password: { type: Types.Password, initial: true, required: true },
+    title: { type: String },
+    image: { type: Types.CloudinaryImage }
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
+    isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+    isPastor: { type: Boolean, label: 'A Pastor', index: true }
 });
 
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function() {
-	return this.isAdmin;
+    return this.isAdmin;
 });
 
 
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,34 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
-	
-	// Render the view
-	view.render('index');
-	
+    
+    var view = new keystone.View(req, res),
+        locals = res.locals;
+    
+    // locals.section is used to set the currently selected
+    // item in the header navigation.
+    locals.section = 'home';
+	locals.data = {
+		pastors: []
+	};
+
+
+    // Load Pastors
+    view.on('init', function(next) {
+    
+        var q = keystone.list('User').model.find()
+            .where('isPastor', 'true')
+            .sort('sortOrder');
+    
+        q.exec(function(err, results) {
+            locals.data.pastors = results;
+            next(err);
+        });
+    
+    });
+    
+    
+    // Render the view
+    view.render('index');
+    
 };
